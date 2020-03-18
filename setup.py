e50f1fde4851f822e8432be661607fc5666f6030
Raheeb Hassan <raheeb998@gmail.com>
2020-03-18 14:43:07 +0600

    Updated setup.py
    - added long description (README)
    - bumped verison to 0.2

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    README = f.read()
 
 setup(
     name='vlawyer',
     packages=['vlawyer'],
-    version='0.1',
+    version='0.2',
     license='gpl-3.0',
     description='An unofficial vjudge.net statistics API',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='Raheeb Hassan',
     author_email='raheeb@myself.com',
     url='https://github.com/hoenchioma/vlawyer',
-    download_url='https://github.com/hoenchioma/vlawyer/archive/v0.1.tar.gz',
     keywords=['vjudge', 'virtualjudge', 'competitive-programming', 'contest'],
     install_requires=['requests'], # package dependencies
     classifiers=[
@@ -19,11 +23,11 @@ setup(
         # Define that your audience are developers
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU Lesser General Public License v3.0',
         # Specify which python versions that you want to support
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
+    python_requires='>=3.5',
 )
